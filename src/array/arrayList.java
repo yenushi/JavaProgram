23d4b272749d44672ba55d314981bf659f80ba7c
IT21170966 <it2117066@my.sliit.lk>
2022-07-08 20:34:28 +0530

    create array in java and display the values

---
+++ src/array/arrayList.java
@@ -0,0 +1,18 @@
+package array;
+import java.util.*; // import for accessing ArrayList Class
+public class arrayList {
+    public static void main(String[] args) {
+        ArrayList<String> nameList = new ArrayList<>(); // create an array list
+
+        // Add items in to the array list
+        nameList.add("Indu");
+        nameList.add("dini");
+        nameList.add("Cham");
+        nameList.add("athka");
+        nameList.add("Thennakoon");
+
+        System.out.println("The size of the array list: "+ nameList.size());
+
+
+    }
+}
