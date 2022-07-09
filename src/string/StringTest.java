058c239a8068b1950186ff1dc4dd08a1b8fef7c7
IT21170966 <it2117066@my.sliit.lk>
2022-07-09 19:09:31 +0530

    printing value of string

--- src/string/StringTest.java
+++ src/string/StringTest.java
@@ -2,13 +2,15 @@ package string;
 
 import java.util.Scanner;
 
-public class String {
+public class StringTest {
+
     public static void main(String[] args) {
         char[] charString = {'S','t', 'r' , 'i' , 'n' , 'g'}; //an array of characters
 
         Scanner scanner = new Scanner (System.in);
-        String string = new newLine(charString); //make string from the characters
+        String string = new String(charString); //make string from the characters
 
+        System.out.println("The value of string is: " + string);
 
     }
 }
