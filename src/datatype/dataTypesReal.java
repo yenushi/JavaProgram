6ee342fe7561e107a4005df2d248492f7294f762
IT21170966 <it2117066@my.sliit.lk>
2022-07-09 23:42:21 +0530

    float data type

---
+++ src/datatype/dataTypesReal.java
@@ -0,0 +1,12 @@
+package datatype;
+
+public class dataTypesReal {
+    public static void main(String[] args) {
+        float myfloat; //floating type data, size 4-bytes
+
+        myfloat = 50.26F; // the letter F or f is needed to represent as float
+        System.out.println("The values of myfloat: " + myfloat);
+
+
+    }
+}
