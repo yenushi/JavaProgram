5db42811e1a4f8e676fec173a5f02f0274ad7cd2
IT21170966 <it2117066@my.sliit.lk>
2022-07-09 21:42:16 +0530

    basic idea of JAVA functions

---
+++ src/functions/Functions.java
@@ -0,0 +1,15 @@
+package functions;
+
+public class Functions {
+
+
+        /* JAVA functions can be categorized in two types
+                1) User defined function
+                2) Inbuilt function / predefined function / library function
+
+         USER DEFINED FUNCTIONS
+                <return_type> function_name (<argument_list>)
+         */
+
+
+}
