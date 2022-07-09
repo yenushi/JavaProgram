75d7d1ef4ee17b2581b8803a0859f65e88e885e5
IT21170966 <it2117066@my.sliit.lk>
2022-07-09 17:34:47 +0530

    edited minus operator

--- src/operators/unaryOperators.java
+++ src/operators/unaryOperators.java
@@ -5,7 +5,7 @@ public class unaryOperators {
         int var1, var2;
         var1 = 50; // assign 50 into the var1 variable
 
-        var2 = -var1; // unary operator to invert the sign of thr value of var1
+        var2 = -var1; // - unary operator to invert the sign of thr value of var1
         System.out.println("The var1 = " + var1 + ", var2 = " + var2);
 
     }
