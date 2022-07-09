35cad4d808a0fc1a009a29534dcf09b095892422
IT21170966 <it2117066@my.sliit.lk>
2022-07-09 17:19:27 +0530

    assigning value to the variable

--- src/operators/unaryOperators.java
+++ src/operators/unaryOperators.java
@@ -1,4 +1,8 @@
 package operators;
 
 public class unaryOperators {
+    public static void main(String[] args) {
+        int var1, var2;
+        var1 = 50; // assign 50 into the var1 variable
+    }
 }
