7bef5d521f60ea64c757fd35e4bbde4688378701
IT21170966 <it2117066@my.sliit.lk>
2022-07-09 18:42:51 +0530

    create character array

---
+++ src/string/String.java
@@ -0,0 +1,12 @@
+package string;
+
+import java.util.Scanner;
+
+public class String {
+    public static void main(String[] args) {
+        char[] charString = {'S','t', 'r' , 'i' , 'n' , 'g'}; //an array of characters
+
+
+
+    }
+}
