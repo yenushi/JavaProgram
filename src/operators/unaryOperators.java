cf8b03f57704cfe8ecfc7b3ee5477dfe6ddd7493
IT21170966 <it2117066@my.sliit.lk>
2022-07-09 18:05:21 +0530

    edited pre-increment operators

--- src/operators/unaryOperators.java
+++ src/operators/unaryOperators.java
@@ -8,7 +8,7 @@ public class unaryOperators {
         var2 = -var1; // - unary operator to invert the sign of thr value of var1
         System.out.println("The var1 = " + var1 + ", var2 = " + var2);
 
-        var2 = ++var1; //pre-incrementing, store previous value , then increase var1 by 1
+        var2 = ++var1; //pre-incrementing, increase var1 by 1, then assign to var2
         System.out.println("The var1 = " + var1 + ", var2 = " + var2);
 
         var2 = var1++;//post-incrementing, increase var1 by 1, then assign to var2
