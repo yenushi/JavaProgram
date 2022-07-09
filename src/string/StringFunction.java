74769fe5df569f40e30a84d755677c96048cccd4
IT21170966 <it2117066@my.sliit.lk>
2022-07-09 19:37:40 +0530

    length of the string

--- src/string/StringFunction.java
+++ src/string/StringFunction.java
@@ -1,9 +1,13 @@
 package string;
 
 public class StringFunction {
-    String str1 = "HELLO";
-    String str2 = "Hello";
+    public static void main(String[] args) {
+        String str1 = "HELLO";
+        String str2 = "Hello";
 
+        // length function to get number of characters in the string
+        System.out.println("The lenght of the string : " + str1 + "is: " +str1.length());
+    }
 
 }
 
