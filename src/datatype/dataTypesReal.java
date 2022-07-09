ad7ecc2aeda54ca5d5ccf2047b7b0515862cf57c
IT21170966 <it2117066@my.sliit.lk>
2022-07-09 23:43:06 +0530

    type casting

--- src/datatype/dataTypesReal.java
+++ src/datatype/dataTypesReal.java
@@ -7,6 +7,8 @@ public class dataTypesReal {
         myfloat = 50.26F; // the letter F or f is needed to represent as float
         System.out.println("The values of myfloat: " + myfloat);
 
+        myfloat = (float) 50/3; //Type casting to change integer to float
+        System.out.println("The value of myfloat: " + myfloat);
 
     }
 }
