a44a0d3dd155d424d40cd6e25fb84cff1f34b4fc
IT21170966 <it2117066@my.sliit.lk>
2022-07-09 23:49:10 +0530

    direct assign

---
+++ src/datatype/DataTypeCharacter.java
@@ -0,0 +1,13 @@
+package datatype;
+
+public class DataTypeCharacter {
+    public static void main(String[] args) {
+        char mychar; //character type data, size 2-byte
+
+        mychar = 'A'; //direct assign character A with single quote
+        System.out.println("The value of mychar: "); // display the value of mychar
+
+
+
+    }
+}
