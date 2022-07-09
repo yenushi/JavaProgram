887b17d44de1fba70d05e5d93fa684a7dc3fd499
IT21170966 <it2117066@my.sliit.lk>
2022-07-09 13:17:17 +0530

    or operator

---
+++ src/conditionlOperators/conditionalOperators.java
@@ -0,0 +1,14 @@
+package conditionlOperators;
+
+public class conditionalOperators {
+    public static void main(String[] args) {
+        boolean bool1,bool2,result;
+        bool1 = true;
+        bool2 = false;
+
+        result = bool1 || bool2; // true when at least one value is true
+        System.out.println("The result is: " + result);
+
+
+    }
+}
