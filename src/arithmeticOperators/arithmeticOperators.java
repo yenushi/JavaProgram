8905dcef0789968b3c04f8d01e90229a6fe51f14
IT21170966 <it2117066@my.sliit.lk>
2022-07-09 11:08:09 +0530

    take two values from user

---
+++ src/arithmeticOperators/arithmeticOperators.java
@@ -0,0 +1,23 @@
+package arithmeticOperators;
+
+import java.util.Scanner;
+
+public class arithmeticOperators {
+    public static void main(String[] args) {
+        int a,b, result;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the value of a and b: ");
+        // take two values from user
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+
+    }
+
+
+
+
+
+
+
+}
