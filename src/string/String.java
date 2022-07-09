023511c5d03981f390a27e8404f398f75eaf8434
IT21170966 <it2117066@my.sliit.lk>
2022-07-09 18:43:42 +0530

    get user input

--- src/string/String.java
+++ src/string/String.java
@@ -6,6 +6,8 @@ public class String {
     public static void main(String[] args) {
         char[] charString = {'S','t', 'r' , 'i' , 'n' , 'g'}; //an array of characters
 
+        Scanner scanner = new Scanner (System.in);
+        String string = new newLine(charString); //make string from the characters
 
 
     }
