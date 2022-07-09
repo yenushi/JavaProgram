d957c8e1478a4be2ef0256a054b37848d8c9ab75
IT21170966 <it2117066@my.sliit.lk>
2022-07-09 17:59:37 +0530

    post-increment operators

--- src/operators/unaryOperators.java
+++ src/operators/unaryOperators.java
@@ -11,10 +11,12 @@ public class unaryOperators {
         var2 = ++var1; //pre-incrementing, store previous value , then increase var1 by 1
         System.out.println("The var1 = " + var1 + ", var2 = " + var2);
 
-        var1++;//post-incrementing, increase var1 by 1, then assign to var2
+        var2 = var1++;//post-incrementing, increase var1 by 1, then assign to var2
         System.out.println("The var1 = " + var1 + ", var2 = " + var2);
 
         var2 = --var1; //pre-decrementing, store previous value , then increase var1 by 1
         System.out.println("The var1 = " + var1 + ", var2 = " + var2);
+
+
     }
 }
