540fc44305693c5eff611fe1e9398272ac99a3bf
IT21170966 <it2117066@my.sliit.lk>
2022-07-09 19:40:13 +0530

    equal method of the string

--- src/string/StringFunction.java
+++ src/string/StringFunction.java
@@ -6,7 +6,15 @@ public class StringFunction {
         String str2 = "Hello";
 
         // length function to get number of characters in the string
-        System.out.println("The lenght of the string : " + str1 + "is: " +str1.length());
+        System.out.println("The length of the string : " + str1 + "is: " +str1.length());
+
+        //equal method to check both strings are same of not (CASE SENSITIVE)
+        if(str1.equals(str2))
+            System.out.println(str1 + " & " + str2 + "are same");
+        else
+            System.out.println(str1 + " & " + str2 + "are not same");
+
+
     }
 
 }
