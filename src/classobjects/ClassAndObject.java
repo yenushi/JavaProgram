41905d3a8aee4fabcd17f3410021106069995724
IT21170966 <it2117066@my.sliit.lk>
2022-07-10 09:04:17 +0530

    comment

--- src/classobjects/ClassAndObject.java
+++ src/classobjects/ClassAndObject.java
@@ -3,14 +3,26 @@ package classobjects;
 //definition of a class named Student
 class Student{
     //member variables of the student class
+
+    /*  these member variables are in the default scope.
+        because here we did not mention them that they are under
+        private or public scope
+    */
+
     String name;
     int id;
     String major;
 
     //member functions/ method declaration
-    void setName(String stName){ // take the student name as string and assign to name variable
+    void setName(String stName){
         name = stName;
+
+        /*  name of the method is setName.
+            And it takes the student name as string and assign to
+            name variable.
+        */
     }
+
 
 }
 
