6267964c098a0513f6a20cc76793bf18aa853558
IT21170966 <it2117066@my.sliit.lk>
2022-07-10 08:28:51 +0530

    display boolean value

---
+++ src/datatype/DataTypeBoolean.java
@@ -0,0 +1,12 @@
+package datatype;
+
+public class DataTypeBoolean {
+    public static void main(String[] args) {
+        boolean mybool; //boolean type data, either true or false
+
+        mybool = true;// set as true
+        System.out.println("The value of mybool: " + mybool); //display the value of my bool
+
+
+    }
+}
