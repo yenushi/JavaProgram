ac80a184933b3dc9465ec1a53733643048430f38
IT21170966 <it2117066@my.sliit.lk>
2022-07-10 22:14:49 +0530

    points to new objects

--- src/classobjects/ClassAndObject.java
+++ src/classobjects/ClassAndObject.java
@@ -39,6 +39,9 @@ public class ClassAndObject {
     public static void main(String[] args) {
         Student s1, s2; // declare two reference variable to point student type object
 
+        s1 = new Student(); // s1 now points to the Student type object
+        s2 = new Student(); // s2 now points to the Student type object
+
     }
 
 }
