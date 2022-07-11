2dc0cd0a827cbfd2d185ba7c2277d8207f355bcc
IT21170966 <it2117066@my.sliit.lk>
2022-07-11 16:17:20 +0530

    create integer set1 and set2

---
+++ src/hash/javaset.java
@@ -0,0 +1,13 @@
+package hash;
+
+import java.util.*;
+
+public class javaset {
+    public static void main(String[] args) {
+        int[] list1 = {1, 5, 2, 6, 5, 2, 12}; // some duplicate data are presented
+        int[] list2 = {12, 4, 3, 2, 4, 15};
+
+        Set<Integer> set1 = new HashSet<Integer>();
+        Set<Integer> set2 = new HashSet<Integer>();
+    }
+}
