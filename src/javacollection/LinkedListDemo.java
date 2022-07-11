a77b901269215d84ab62032c6d57693e38162792
IT21170966 <it2117066@my.sliit.lk>
2022-07-11 12:13:27 +0530

    create a Linked list

---
+++ src/javacollection/LinkedListDemo.java
@@ -0,0 +1,12 @@
+package javacollection;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        //create a linked list for integer type data
+        LinkedList<Integer> intList = new LinkedList<>();
+
+    }
+}
