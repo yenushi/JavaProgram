2263ee1df7ab104f2b1d59a82392ebf6d3e816f3
IT21170966 <it2117066@my.sliit.lk>
2022-07-11 12:39:11 +0530

    create a array list

--- src/javacollection/LinkedListDemo.java
+++ src/javacollection/LinkedListDemo.java
@@ -1,5 +1,6 @@
 package javacollection;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -14,6 +15,13 @@ public class LinkedListDemo {
         intList.add(30);
         intList.add(40);
         intList.add(50);
+
+        System.out.println("The linked list size: " + intList.size() + "\ncontent: " + intList + "\n");
+
+        int[] array = { 5, 15 , 30, 80 , 60,25}; // an array of integers
+        // create another array list with all the item in the list
+        ArrayList<Integer> arrList = new ArrayList<>();
+
 
 
 
