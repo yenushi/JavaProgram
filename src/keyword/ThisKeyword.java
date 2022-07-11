dbaabd28931dc92149bbad761bbd91c5d6a5d669
IT21170966 <it2117066@my.sliit.lk>
2022-07-11 11:46:25 +0530

    use 'this' keyword

--- src/keyword/ThisKeyword.java
+++ src/keyword/ThisKeyword.java
@@ -2,6 +2,11 @@ package keyword;
 
 class Myclass{
     private int a, b;
+
+    public Myclass(int a, int b){ // constructor parameter name same as member variables
+        this.a = a; // this.a points the member variables a. (a=a) is ambiguous, so this keyword is used
+        this.b = b;
+    }
 }
 
 public class ThisKeyword {
